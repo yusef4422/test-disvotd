fedbcedea579b3ce654301790468e9e6341dd50f
nawaf112344 <47340062+nawaf112344@users.noreply.github.com>
2019-02-08 17:33:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,61 +1,45 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs")
-const client = new Discord.Client();
-client.on('ready', () => {
-    console.log(`Fkk Game Started By Friends Team`);
-    client.user.setStatus("dnd")
+const cuttweet = [
+     'كت تويت ‏| تخيّل لو أنك سترسم شيء وحيد فيصبح حقيقة، ماذا سترسم؟',
+     'كت تويت | أكثر شيء يُسكِت الطفل برأيك؟',
+     'كت تويت | الحرية لـ ... ؟',
+     'كت تويت | قناة الكرتون المفضلة في طفولتك؟',
+     'كت تويت ‏| كلمة للصُداع؟',
+     'كت تويت ‏| ما الشيء الذي يُفارقك؟',
+     'كت تويت | موقف مميز فعلته مع شخص ولا يزال يذكره لك؟',
+     'كت تويت ‏| أيهما ينتصر، الكبرياء أم الحب؟',
+     'كت تويت | بعد ١٠ سنين ايش بتكون ؟',
+     'كت تويت ‏| مِن أغرب وأجمل الأسماء التي مرت عليك؟',
+     '‏كت تويت | عمرك شلت مصيبة عن شخص برغبتك ؟',
+     'كت تويت | أكثر سؤال وجِّه إليك مؤخرًا؟',
+     '‏كت تويت | ما هو الشيء الذي يجعلك تشعر بالخوف؟',
+     '‏كت تويت | وش يفسد الصداقة؟',
+     '‏كت تويت | شخص لاترفض له طلبا ؟',
+     '‏كت تويت | كم مره خسرت شخص تحبه؟.',
+     '‏كت تويت | كيف تتعامل مع الاشخاص السلبيين ؟',
+     '‏كت تويت | كلمة تشعر بالخجل اذا قيلت لك؟',
+     '‏كت تويت | جسمك اكبر من عٌمرك او العكسّ ؟!',
+     '‏كت تويت |أقوى كذبة مشت عليك ؟',
+     '‏كت تويت | تتأثر بدموع شخص يبكي قدامك قبل تعرف السبب ؟',
+     'كت تويت | هل حدث وضحيت من أجل شخصٍ أحببت؟',
+     '‏كت تويت | أكثر تطبيق تستخدمه مؤخرًا؟',
+     '‏كت تويت | ‏اكثر شي يرضيك اذا زعلت بدون تفكير ؟',
+     '‏كت تويت | وش محتاج عشان تكون مبسوط ؟',
+     '‏كت تويت | مطلبك الوحيد الحين ؟',
+     'كت تويت | بروبوت ولا فايبر بوت',
+]
+ 
+ client.on('message', message => {
+   if (message.content.startsWith("#كت")) {
+                if(!message.channel.guild) return message.reply(' This command only for servers');
+  var embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+ .addField('لعبه كت تويت' ,
+  ${cuttweet[Math.floor(Math.random() * cuttweet.length)]})
+  message.channel.sendEmbed(embed);
+    }
 });
-let points = JSON.parse(fs.readFileSync('./Data/fkkPTS.json', 'utf8'));
-const prefix = "#";//البريفكس
-
-client.on('message', message => {
-if (!points[message.author.id]) points[message.author.id] = {
-	points: 0,
-  };
-if (message.content.startsWith(prefix + 'فكك')) {
-	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
-
-const type = require('./Data/fkk.json');
-const item = type[Math.floor(Math.random() * type.length)];
-const filter = response => {
-    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-};
-message.channel.send('**لديك 15 ثانيه لتفكيك الكلمه**').then(msg => {
-
-			
-msg.channel.send(`${item.type}`).then(() => {
-        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
-        .then((collected) => {
-		message.channel.send(`${collected.first().author} ✅ **احسنت لقد تمكنت من تفكيك الكلمه بسرعه**`);
-		console.log(`[Typing] ${collected.first().author} typed the word.`);
-            let won = collected.first().author;
-            points[won.id].points++;
-          })
-          .catch(collected => {
-            message.channel.send(`:x: **لم يتمكن احد من تفكيك الكلمه في الوقت المناسب**`);
-			console.log('[Typing] Error: No one type the word.');
-          })
-		})
-	})
-}
-});
-client.on('message', message => {
-if (message.content.startsWith(prefix + 'نقاطي')) {
-	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
-	let userData = points[message.author.id];
-	let embed = new Discord.RichEmbed()
-    .setAuthor(`${message.author.tag}`, message.author.avatarURL)
-	.setColor('#000000')
-	.setDescription(`نقاطك: \`${userData.points}\``)
-	message.channel.sendEmbed(embed)
-  }
-  fs.writeFile("./Data/fkkPTS.json", JSON.stringify(points), (err) => {
-    if (err) console.error(err)
-  })
-});
-
-
 
 client.on('ready', () => {
    console.log(`----------------`);
