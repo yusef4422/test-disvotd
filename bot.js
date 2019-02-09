6bb872ea6d9bcbff33600c7b342a00094c95e28a
nawaf112344 <47340062+nawaf112344@users.noreply.github.com>
2019-02-09 06:54:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,61 @@
-
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-client.user.setActivity('آترك رسالتك عبر الخآص', { type: 'LISTENING' })
 
+client.on('ready', () => {
+  console.log(`Invitations Code Started By Friends Team`);
+    client.user.setStatus("dnd")
 });
+
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("543624805379866639")// ايدي السيرفر
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("543624805379866645");// ايدي الروم
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("543624805379866639")// ايدي السيرفر
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`**ارحب يا __<@${member.id}>__ في سيرفر __${member.guild.name}__\nتم دعوته من قبل  ${Invite.inviter}\nالرابط اللي دخل منه : https://discord.gg/${Invite.code}**`)
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+
+client.on('message', message => {
+    if (message.content.startsWith("invite")) {
+    message.guild.fetchInvites()
+    .then(invites => message.channel.send(`**انت جبت  ${invites.find(invite => invite.inviter.id === null ? "0" : message.author.id).uses} **`))
+     
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
