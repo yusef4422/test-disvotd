22d1ae7bdd1dea89668025889a642686a49fb916
nawaf112344 <47340062+nawaf112344@users.noreply.github.com>
2019-02-09 07:50:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -342,14 +342,14 @@ client.on('message',async message => {
     if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
     if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
     message.channel.send('✅| **تم عمل الروم بنجاح**');
-    message.guild.createChannel(`Voice Quiet : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
-      console.log(`Voice Quiet channel setup for guild: \n ${message.guild.name}`);
+    message.guild.createChannel(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
+      console.log(`Voice Online channel setup for guild: \n ${message.guild.name}`);
       c.overwritePermissions(message.guild.id, {
         CONNECT: false,
         SPEAK: false
       });
       setInterval(function() {
-        c.setName(`Voice Quiet : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]`)
+        c.setName(`Voice Online: 「 ${message.guild.members.filter(m => m.voiceChannel).size} 」`)
       },1000);
     });
     }
